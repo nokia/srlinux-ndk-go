88e29ed30d135f35f724f2bbed3cb7f3dbd085a5
Roman Dodin <dodin.roman@gmail.com>
2023-11-12 01:03:23 +0200

    v0.2.0-rc1 release

--- gen.sh
+++ gen.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+# usage: bash gen.sh <protobufVersion>, where protobufVersion is the tag of the protobufs repo
+# example: bash gen.sh v0.2.0
 
 set -e
 
@@ -9,7 +12,7 @@ PROJ_DIR=$(pwd)
 PROTO_DIR=~/nokia/srlinux-ndk-protobufs
 
 # tag matching the protobuf tag in the https://github.com/nokia/srlinux-ndk-protobufs
-# from which the Go bindings are about to be generated
+# from which the Python bindings are about to be generated
 PROTO_VER=$1
 
 if [ -z "$1" ]; then
@@ -17,8 +20,18 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+# remove previously generated bindings
+rm -rf ${PROJ_DIR}/ndk
+
 # checkout protos to the desired version
 cd ${PROTO_DIR} && git checkout ${PROTO_VER}
 
-docker run -v ${PROTO_DIR}:/in -v ${PROJ_DIR}:/out ghcr.io/srl-labs/protoc:0.0.2 \
-  bash -c "protoc --go_out=paths=source_relative:/out --go-grpc_out=paths=source_relative:/out ndk/*.proto"
+docker run -v ${PROTO_DIR}:/in -v ${PROJ_DIR}:/out ghcr.io/srl-labs/protoc:23.3__1.31.0 \
+  ash -c "protoc --go_out=paths=source_relative:/out --go_opt=paths=source_relative --go-grpc_out=paths=source_relative:/out --go-grpc_opt=paths=source_relative,require_unimplemented_servers=false ndk/*.proto"
+
+# once the bindings are generated, we can push it to the repo
+# git push
+# and create a release
+# it is safer to create an rc release first, and then promote it to a stable release
+# once the apps are tested to work with it
+# gh release create v0.2.0-rc1 --generate-notes
--- go.mod
+++ go.mod
@@ -1,8 +1,16 @@
 module github.com/nokia/srlinux-ndk-go
 
-go 1.16
+go 1.19
 
 require (
-	google.golang.org/grpc v1.42.0
-	google.golang.org/protobuf v1.27.1
+	google.golang.org/grpc v1.56.3
+	google.golang.org/protobuf v1.30.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
 )
